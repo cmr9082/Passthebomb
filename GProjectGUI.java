496d1ef28a1c5a13f980e698f2d1cdce0cfcd86d
cmr9082 <82056332+cmr9082@users.noreply.github.com>
2022-04-05 17:48:41 -0400

    Add files via upload

---
+++ GProjectGUI.java
@@ -0,0 +1,268 @@
+import javafx.application.Application;
+import javafx.event.*;
+import javafx.scene.*;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.stage.*;
+import javafx.geometry.*;
+import javafx.scene.image.*;
+import java.net.*;
+import java.io.*;
+import java.util.*;
+
+/**
+ * GUIStarter - class to help with JavaFX classes
+ * @author  D. Patric
+ * @version 2205
+ */
+
+public class GProjectGUI extends Application implements EventHandler<ActionEvent> {
+   //General Declarations
+   private Stage stage;       
+   private Socket socket = null;
+   private PrintWriter pw = null;
+   private Scanner scn = null;
+   
+   
+   //Start Menu Initiations
+   private Scene sceneStart;
+   private VBox rootStart = new VBox(8);
+   private FlowPane fpMenu0 = new FlowPane(10,10);
+   private FlowPane fpMenu1 = new FlowPane(50,30);
+   private Button btnHTP = new Button("How to Play");
+   private Button btnJoin = new Button("Join");
+   private Image title = new Image("/title.png",true);
+   private ImageView titleView = new ImageView(title);
+   private TextInputDialog tidName = new TextInputDialog();
+   private TextInputDialog tidIP = new TextInputDialog();   
+   
+   
+   //Game Menu Intiations
+   private Scene sceneGame;   
+   private FlowPane rootGame = new FlowPane(10,10);
+   private VBox vbGame1 = new VBox(8);
+   private VBox vbGame2 = new VBox(8);
+   private FlowPane fpChatSend = new FlowPane(5,5);
+   private FlowPane fpNext = new FlowPane(5,5);
+   
+   private ProgressBar timer = new ProgressBar();
+   private TextField currentWord = new TextField();
+   private Button btnNext = new Button("Next");
+   private TextArea taChat = new TextArea();
+   private TextField tfChatInput = new TextField();
+   private Button btnSend = new Button("Send");
+   
+   private TextArea taList = new TextArea();
+   private Button btnLeave = new Button("Leave");
+   private Button btnGrab = new Button("Grab Host");
+   private Button btnStart = new Button("Start Game");
+   private ComboBox cbCategory = new ComboBox();
+   private Button btnRelinquish = new Button("Relinquish Host");
+   private Label lblHost = new Label("Host Controls:");
+   
+
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   // Main just instantiates an instance of this GUI class
+   public static void main(String[] args) {
+      launch(args);
+   }
+   
+   // Called automatically after launch sets up javaFX
+   public void start(Stage _stage) throws Exception {
+      //Initial Setups
+      stage = _stage;                      
+      stage.setTitle("Pass The Bomb");            
+      sceneStart = new Scene(rootStart, 500, 500);
+      sceneGame = new Scene(rootGame, 700, 600);                             
+      stage.setScene(sceneStart);                 
+      stage.show();                          
+      
+      
+      //Setting Up Start Menu
+      titleView.setFitWidth(250);
+      titleView.setFitHeight(250);
+      fpMenu1.getChildren().addAll(btnJoin,btnHTP);
+      fpMenu1.setAlignment(Pos.CENTER);
+      fpMenu0.getChildren().add(titleView);
+      fpMenu0.setAlignment(Pos.CENTER);
+      rootStart.getChildren().addAll(fpMenu0,fpMenu1);
+      tidName.setHeaderText("Enter Your Username");
+      tidName.setContentText("Name: ");
+      tidIP.setHeaderText("Enter the Server's IP Address");
+      tidIP.setContentText("Server IP: ");         
+      btnJoin.setOnAction(this);
+      btnHTP.setOnAction(this);
+      
+      //Setting Up Game Menu
+      timer.setProgress(1.0F);
+      rootGame.getChildren().addAll(vbGame1,vbGame2);
+      fpNext.getChildren().add(btnNext);
+      fpChatSend.getChildren().addAll(tfChatInput,btnSend);
+      vbGame1.getChildren().addAll(timer,currentWord,fpNext,taChat,fpChatSend);
+      vbGame2.getChildren().addAll(taList,btnLeave,lblHost,btnGrab,btnStart,cbCategory,btnRelinquish);
+      fpNext.setAlignment(Pos.CENTER);
+      taList.setPrefWidth(250);
+      taChat.setPrefWidth(400);
+      timer.setPrefWidth(400);
+      tfChatInput.setPrefWidth(350);
+      currentWord.setEditable(false);
+      taList.setEditable(false);
+      taChat.setEditable(false);
+      btnStart.setDisable(true);
+      btnRelinquish.setDisable(true);
+      cbCategory.setDisable(true);
+      cbCategory.getItems().addAll("Everyday Objects","Phrases","Activities","Brands","Video Games","Movies","Foods");
+      btnLeave.setOnAction(this);
+      btnNext.setOnAction(this);
+      btnSend.setOnAction(this);
+      btnGrab.setOnAction(this);
+      btnStart.setOnAction(this);
+      btnRelinquish.setOnAction(this);
+      
+
+
+      
+      
+      
+      
+   }
+   
+   public void handle(ActionEvent evt) {
+      // Get the button that was clicked
+      Button btn = (Button)evt.getSource();
+      
+      // Switch on its name
+      switch(btn.getText()) {
+          case "Join":
+              doConnect();
+             stage.setScene(sceneGame);
+             break;
+          case "How to Play":
+             //stage.setScene(sceneHTP);
+             System.out.println("How To Play screen starts");  
+             break;
+          case "Leave":
+            stage.setScene(sceneStart);
+            //socket.close();
+            break;
+          case "Grab Host":
+             btnStart.setDisable(false);
+             btnRelinquish.setDisable(false);
+             cbCategory.setDisable(false);
+             btnGrab.setDisable(true); 
+             break;   
+          case "Relinquish Host":
+             btnStart.setDisable(true);
+             btnRelinquish.setDisable(true);
+             cbCategory.setDisable(true);
+             cbCategory.setValue("");
+             btnGrab.setDisable(false); 
+             break;               
+          case "Start":
+            //Theoretically would start another method maybe in another class or sumthin
+            break;
+     
+      }
+      
+      
+      //Recieving
+           
+             /*while(scn.hasNextLine()) {
+               String command = scn.nextLine();
+                switch(command) {
+                  case "UPDATE-PLAYER-LIST":
+                     while(scn.hasNextLine()) {
+                        taList.appendText(scn.nextLine()+"\n");
+                     }
+                     break;
+                  default:
+                     pw.println("ERROR-Unrecognized command: " + command);
+                     pw.flush();
+                     break;
+               }  // switch 
+            }*/
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+   }
+   
+
+   
+   
+//Sending   
+   public void doConnect(){
+             tidIP.showAndWait();
+             tidName.showAndWait();
+             String ip = (String)tidIP.getResult();
+             String name = (String)tidName.getResult();
+             try{socket = new Socket(ip, 45549);
+                scn = new Scanner(socket.getInputStream());
+                pw = new PrintWriter(socket.getOutputStream());  
+                pw.println("JOIN");
+                pw.println(name);
+                pw.flush();
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                //Problem Area while loop creates hanging
+                
+                while(true){
+                System.out.print(scn.nextLine());
+                }
+                
+                
+                
+                
+                
+                
+                
+       
+                   
+             }catch(Exception e){System.out.println("Problem joining: "+ e.getMessage());taChat.setText("Error Joining: Leave this Lobby and try rejoining with correct ip");}
+             
+  
+   
+   }
+   
+   
+   
+   
+   
+   
+   
+      
+}	
---
+++ GProjectServer.java
@@ -0,0 +1,183 @@
+import javafx.application.*;
+import javafx.event.*;
+import javafx.scene.*;
+import javafx.scene.control.*;
+import javafx.scene.control.Alert.*;
+import javafx.scene.text.*;
+import javafx.scene.layout.*;
+import javafx.stage.*;
+import javafx.geometry.*;
+import java.net.*;
+import java.io.*;
+import java.util.*;
+
+public class GProjectServer extends Application implements EventHandler<ActionEvent>{
+   // Window attributes
+   private Stage stage;
+   private Scene scene;
+   private VBox root = new VBox();
+   private FlowPane fp1 = new FlowPane();
+
+   // GUI Components
+   private Label lblServer = new Label("Server IP:");
+   private TextField tfServer = new TextField();
+   private TextArea taLog = new TextArea();
+   
+   //Variables
+   private Vector<String> playerList = new Vector<String>();
+   
+   
+   
+   
+   
+   
+   
+   public static void main(String[] args) {
+      launch(args);
+       
+   }
+   
+   /**
+    * Launch, draw and set up GUI
+    * Do server stuff
+    */
+    
+   public void start(Stage _stage) {
+      // Window setup
+      stage = _stage;
+      stage.setTitle("Pass the Bomb SERVER");
+      stage.setOnCloseRequest(new EventHandler<WindowEvent>() {public void handle(WindowEvent evt) {System.exit(0);}});
+      stage.setResizable(false);
+      scene=new Scene(root, 500, 500); 
+      stage.setScene(scene);                 
+      stage.show();
+      fp1.getChildren().addAll(lblServer,tfServer);
+      root.getChildren().addAll(fp1,taLog);
+      tfServer.setEditable(false);
+      taLog.setEditable(false);
+      taLog.setPrefHeight(450);
+      
+      try{tfServer.appendText(InetAddress.getLocalHost().getHostAddress().trim());}catch(Exception e){taLog.appendText("Error Getting Local IP: "+e.getMessage()+"\n");}
+      
+      
+      
+      Thread t1 = new Thread() {
+         public void run() {
+            doServerStuff();
+         }
+      };
+      t1.start();
+            
+         }
+      
+      
+      
+      
+      
+      
+   
+      public void handle(ActionEvent evt) {}
+     private void doServerStuff() {
+      try {
+         ServerSocket sSocket = new ServerSocket(45549);
+         while(true) {
+            Socket socket1 = sSocket.accept();
+            Thread t2 = new ClientThread(socket1);
+            t2.start();
+         }
+      }
+      catch(Exception e) {
+      }
+   }
+   
+   
+   
+   
+   
+   
+   
+   
+  class ClientThread extends Thread {
+      Socket socket2 = null;
+      Scanner scn = null;
+      PrintWriter pw = null;
+
+      
+      /** constructor */
+      public ClientThread(Socket _cSocket) {
+         socket2 = _cSocket;
+      }
+      
+      /** Thread main */
+      public void run() {
+         try {
+            scn = new Scanner(socket2.getInputStream());
+            pw = new PrintWriter(socket2.getOutputStream());
+            taLog.appendText("Request received from " + socket2.getInetAddress().getHostName()+"\n");
+            while(scn.hasNextLine()) {
+               String command = scn.nextLine();
+                switch(command) {
+                  case "JOIN":
+                     String name = scn.nextLine();
+                     taLog.appendText("Player "+name+" has Joined\n");
+                     playerList.add(name);
+                     pw.println("UPDATE-PLAYER-LIST");
+                     pw.flush();
+                     for(int i=0;i<playerList.size();i++){
+                        pw.println(playerList.get(i)+"\n");
+                        pw.flush();
+                        
+                     
+                     }
+                     
+                     break;
+                  default:
+                     pw.println("ERROR-Unrecognized command: " + command);
+                     pw.flush();
+                     break;
+               }  // switch 
+            }  //while
+            
+            pw.close();
+            scn.close();
+            socket2.close();
+         }  // try
+         catch(Exception e) {
+         }
+      }  // run
+      
+           
+    }
+   
+   
+   
+   
+   
+      
+}
+
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+
+       
+  
+   
+   
+   
